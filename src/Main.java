9d0a1b79515717571eb769f0081e3d547b8cca5d
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-06-05 16:46:44 +0800

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "M";
+        String namaTengah = "Wahyudin";
+        String namaBelakang = "Akbar";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.78;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + " " + namaTengah + " " + namaBelakang);
+    }
+}
