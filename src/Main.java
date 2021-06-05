4223254ce93853b6ecd2a67737bf6d81c13042cb
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-06-05 19:48:46 +0800

    Operasi Tipe String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "M";
@@ -14,5 +16,16 @@ public class Main {
         char[] wahyuChar = {'W', 'A', 'H', 'Y', 'U'};
         String wahyuString = new String(wahyuChar);
         System.out.println(wahyuString);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("Wah"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5, 12));
+        System.out.println(namaLengkap.replace("M", "Muhammad"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
     }
 }
