64244cb29b663961e5e5ce9837c5f4e3ef2f9409
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-06-06 18:45:13 +0800

    Membuat Constructor

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -8,14 +9,13 @@ public class Mahasiswa {
     String npm;
     String nama;
     Date tanggalLahir;
+    final String POLA_TANGGAL = "dd-MM-yyyy";
+    final SimpleDateFormat SDF = new SimpleDateFormat(POLA_TANGGAL);
 
     void tampilkanAtribut() {
-        String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
-
         System.out.println("Nama \t\t\t : " + this.nama);
         System.out.println("NPM \t\t\t : " + this.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(this.tanggalLahir));
+        System.out.println("Tanggal Lahir \t : " + SDF.format(this.tanggalLahir));
     }
 
     void menyapa() {
@@ -38,5 +38,15 @@ public class Mahasiswa {
         }
 
         return selisihTahun;
+    }
+
+    public Mahasiswa(String npm, String nama, String tanggalLahir) {
+        this.npm = npm;
+        this.nama = nama;
+        try {
+            this.tanggalLahir = SDF.parse(tanggalLahir);
+        } catch (ParseException e) {
+            System.err.println("Kesalahan pada tanggal lahir");
+        }
     }
 }
--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -1,31 +1,16 @@
 package model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class MahasiswaBeraksi {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
-        String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
-
-        Mahasiswa mahasiswa = new Mahasiswa();
-        mahasiswa.npm = "19630382";
-        mahasiswa.nama = "M Wahyudin Akbar";
-        mahasiswa.tanggalLahir = simpleDateFormat.parse("31-07-2001");
-
+        Mahasiswa mahasiswa = new Mahasiswa("19630382", "M Wahyudin Akbar", "31-07-2001");
         mahasiswa.tampilkanAtribut();
         mahasiswa.menyapa();
         System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " Tahun");
 
         System.out.println();
 
-        Mahasiswa mahasiswa2 = new Mahasiswa();
-        mahasiswa2.npm = "19631234";
-        mahasiswa2.nama = "Akbar";
-        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
-
+        Mahasiswa mahasiswa2 = new Mahasiswa("19631234", "Pratama", "5-03-2003");
         mahasiswa2.tampilkanAtribut();
     }
 }
