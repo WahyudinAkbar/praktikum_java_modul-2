5982d535145942199f616aeb870689a7138142a2
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-06-07 15:35:48 +0800

    Enkapsulasi

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,11 @@
+import model.Mahasiswa;
+
 import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        String namaDepan = "M";
-        String namaTengah = "Wahyudin";
-        String namaBelakang = "Akbar";
-        int usia = 20;
-        int targetTahunKuliah = 4;
-        double ipk = 3.78;
-        char nilaiAbjad = 'A';
-        boolean tampan = true;
-
-        System.out.println(namaDepan + " " + namaTengah + " " + namaBelakang);
-
-        char[] wahyuChar = {'W', 'A', 'H', 'Y', 'U'};
-        String wahyuString = new String(wahyuChar);
-        System.out.println(wahyuString);
-
-        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
-
-        System.out.println(namaLengkap.length());
-        System.out.println(namaLengkap.indexOf("Wah"));
-        System.out.println(namaLengkap.substring(5));
-        System.out.println(namaLengkap.substring(5, 12));
-        System.out.println(namaLengkap.replace("M", "Muhammad"));
-        System.out.println(namaLengkap.toUpperCase());
-        System.out.println(namaLengkap.toLowerCase());
-        System.out.println(namaLengkap.charAt(6));
+        Mahasiswa m1 = new Mahasiswa("19634321", "Dodoy", "07-08-2004");
+        m1.tampilkanAtribut();
+        System.out.println(m1.getNama());
     }
 }
--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -12,10 +12,34 @@ public class Mahasiswa {
     final String POLA_TANGGAL = "dd-MM-yyyy";
     final SimpleDateFormat SDF = new SimpleDateFormat(POLA_TANGGAL);
 
-    void tampilkanAtribut() {
+    public void tampilkanAtribut() {
         System.out.println("Nama \t\t\t : " + this.nama);
         System.out.println("NPM \t\t\t : " + this.npm);
         System.out.println("Tanggal Lahir \t : " + SDF.format(this.tanggalLahir));
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
     }
 
     void menyapa() {
